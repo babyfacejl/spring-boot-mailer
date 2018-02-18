59293150cfdcae9aeca9b5352798160ef2d3fdee
John Luo <johnluo_base@yahoo.com.au>
2018-02-19 01:48:23 +1100

    Updated unit tests

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # spring-boot-mailer
+
+This project was generated with spring boot. This project contains the backend implementation of sending email using Mailgun as primary and Sendgrid as backup Web api.
+
+
+## Build
+
+Run `mvn package` to build the project. The build artifacts will be stored in the `target/` directory. 
+
+## Running unit tests
+
+Run `mvn test` to execute the unit tests .
+
--- src/main/java/com/project/springbootmailer/SpringBootMailerApplication.java
+++ src/main/java/com/project/springbootmailer/SpringBootMailerApplication.java
@@ -1,5 +1,6 @@
 package com.project.springbootmailer;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -7,7 +8,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 @SpringBootApplication
 @EnableCircuitBreaker
 public class SpringBootMailerApplication {
-
+	final static Logger logger = Logger.getLogger(SpringBootMailerApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMailerApplication.class, args);
 	}
